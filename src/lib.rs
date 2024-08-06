b31149b16b624921954ddc1765048b70b4379479
quentingruber <quentingruber@gmail.com>
2024-08-06 15:59:08 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
+use core::panic;
+use std::sync::{
+    atomic::{AtomicPtr, Ordering},
+    Arc, Mutex,
+};
+
 use bevy_ecs::prelude::*;
 use wasm_bindgen::prelude::*;
 #[derive(Component)]
-#[wasm_bindgen]
 struct Position(Vec<f32>);
 
+#[derive(Component)]
+struct H1emuId(String);
 #[derive(Component)]
 struct ZombieEntity();
 #[derive(Component)]
@@ -12,27 +19,48 @@ struct PlayerEntity();
 struct DeerEntity();
 
 #[derive(Component)]
-#[wasm_bindgen]
 struct EntityDefaultBundle {
+    h1emu_id: H1emuId,
     position: Position,
     velocity: Velocity,
+    cb: CB,
 }
 #[derive(Component)]
-#[wasm_bindgen]
 struct Velocity(Vec<f32>);
 
-#[wasm_bindgen]
-struct CB(js_sys::Function);
+#[derive(Component)]
+struct CB(Arc<AtomicPtr<js_sys::Function>>);
 
-#[derive(Resource)]
-struct StupidValue(u32);
+impl CB {
+    // Safely call the JavaScript function stored in the AtomicPtr
+    fn call_js_function(&self, arg: &JsValue) {
+        panic!("fuck");
+        // Load the raw pointer
+        let ptr = self.0.load(Ordering::SeqCst);
+
+        // Check if the pointer is null
+        if !ptr.is_null() {
+            // Convert the raw pointer to a reference
+            unsafe {
+                let js_func: &js_sys::Function = &*ptr;
+                // Call the JavaScript function
+                js_func.call1(&JsValue::NULL, arg).unwrap();
+            }
+        } else {
+            panic!("fuck")
+        }
+    }
+}
 
 // This system moves each entity with a Position and Velocity component
-fn movement(mut query: Query<(&mut Position, &Velocity)>) {
-    for (mut position, velocity) in &mut query {}
-}
-fn test(mut tet: ResMut<StupidValue>) {
-    tet.0 = 2;
+fn movement(mut query: Query<&CB>) {
+    // ZEBI pk la query est viiiide
+    query.get_single().unwrap();
+
+    for cb in &mut query {
+        panic!("fuck");
+        cb.call_js_function(&JsValue::from_str(&"hey"))
+    }
 }
 #[wasm_bindgen]
 pub struct AiManager {
@@ -41,6 +69,7 @@ pub struct AiManager {
 }
 #[wasm_bindgen]
 pub struct EntityFromJs {
+    h1emu_id: String,
     entity_type: EntityType,
     position: Vec<f32>,
     velocity: Vec<f32>,
@@ -54,8 +83,10 @@ impl EntityFromJs {
         position: Vec<f32>,
         velocity: Vec<f32>,
         action_cb: js_sys::Function,
+        h1emu_id: String,
     ) -> EntityFromJs {
         EntityFromJs {
+            h1emu_id,
             entity_type,
             position,
             velocity,
@@ -74,28 +105,22 @@ pub enum EntityType {
 impl AiManager {
     #[wasm_bindgen(constructor)]
     pub fn initialize() -> AiManager {
-        let mut world = World::new();
-        world.insert_resource(StupidValue(0));
+        let world = World::new();
         let mut schedule = Schedule::default();
         schedule.add_systems(movement);
-        schedule.add_systems(test);
 
         AiManager { world, schedule }
-    }
-
-    pub fn getdata(&mut self) -> u32 {
-        let d: &StupidValue = self.world.resource();
-        d.0
     }
 
     pub fn run(&mut self) {
         self.schedule.run(&mut self.world);
     }
-    pub fn add_entity(&mut self, e: EntityFromJs) {
-        e.action_cb.call0(&JsValue::null()).unwrap();
+    pub fn add_entity(&mut self, mut e: EntityFromJs) {
         let mut entity = self.world.spawn(EntityDefaultBundle {
+            h1emu_id: H1emuId(e.h1emu_id),
             position: Position(e.position),
             velocity: Velocity(e.velocity),
+            cb: CB(Arc::new(AtomicPtr::new(&mut e.action_cb))),
         });
         match e.entity_type {
             EntityType::Player => entity.insert(PlayerEntity {}),
