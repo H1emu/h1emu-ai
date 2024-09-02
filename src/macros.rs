09bbd7771453d8f388eb1a9ff5eaf0bcf25b3c6a
quentingruber <quentingruber@gmail.com>
2024-09-02 18:19:58 +0200

    disable logs in release

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,13 @@
 #[macro_export]
+#[cfg(debug_assertions)]
 macro_rules! log {
     ($($t:tt)*) => {
         web_sys::console::log_1(&format!("{:?}",$($t)*).into())
     };
+}
+
+#[macro_export]
+#[cfg(not(debug_assertions))]
+macro_rules! log {
+    ($($t:tt)*) => {};
 }
